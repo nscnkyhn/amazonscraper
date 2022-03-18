75330d2808336d291d36860c6920b270631bd69d
Enes <kayhanenescan@gmail.com>
2022-03-18 14:25:27 +0300

    first commit to upload files

---
+++ aiohttp_parser.py
@@ -0,0 +1,139 @@
+import time
+import asyncio
+import aiohttp
+import random
+import requests
+from bs4 import BeautifulSoup
+
+def get_HEADERS():
+    UA_LIST = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        ]
+    HEADERS = {
+        'authority': 'www.amazon.com.tr',
+        'method': 'GET',
+        'path': '/rd/uedata?at&v=0.223517.0&id=M6NAN2DTPCJXG008A7QB&m=1&sc=csa:lcp&lcp=503&pc=2255&at=2255&t=1647548558895&pty=Search&spty=List&pti=undefined&tid=JJJEYP079ZPSWBKQEAE8&aftb=1',
+        'scheme': 'https',
+        'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'downlink': '5',
+        'ect': '4g',
+        'rtt': '100',
+        'sec-ch-ua': '"Opera GX";v="83", "Chromium";v="97", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'image',
+        'sec-fetch-mode': 'no-cors',
+        'sec-fetch-site': 'same-origin',
+        'User-Agent': random.choice(UA_LIST)
+        }
+    return HEADERS
+
+def number_of_pages(URL):
+    RESPONSE = requests.get(URL, headers=get_HEADERS())
+    TOTAL_NUMBER_OF_PAGES = BeautifulSoup(RESPONSE.text, "html.parser").find('span', class_='s-pagination-item s-pagination-disabled').text
+    return int(TOTAL_NUMBER_OF_PAGES)
+
+def generating_urls(URL, TOTAL_NUMBER_OF_PAGES):
+    #print("Kullanılacak URL'ler üretiliyor.")
+#   Generates the urls of the wanted pages to scrape
+    PAGE_INDEX_OF_URL = URL.index("&page=")
+    URL_PART_1 = URL[:int(PAGE_INDEX_OF_URL+6)]
+    URL_PART_2 = URL[int(PAGE_INDEX_OF_URL+7):]
+    del PAGE_INDEX_OF_URL
+    for i in range(1, int(TOTAL_NUMBER_OF_PAGES)+1):
+        URLS.append(URL_PART_1 + str(i) + URL_PART_2)
+    del URL_PART_1, URL_PART_2
+
+async def async_main():
+    async with aiohttp.ClientSession(headers=get_HEADERS()) as SESSION:
+        for FINAL_URL in URLS:
+            async with SESSION.get(FINAL_URL) as RESPONSE:
+                RESULTS.append(await RESPONSE.text())
+        return RESULTS
+
+
+def listing_cards(RESULTS):
+#   print("Sonuçlar ayıklanıyor")
+#   Filtering htmls and saving product cards.
+    for RESULT in RESULTS: 
+        CARDS.extend(BeautifulSoup(RESULT, features="html.parser").find_all('div', {'data-asin': True,  'data-component-type': 's-search-result'}))
+    return CARDS
+
+def processing_cards(CARDS):
+#   Processing cards and filtering title, data-asin and price values.
+    #print("Ayıklanan ürünler işleniyor.")
+    for CARD in CARDS:
+        try:
+            TITLE = CARD.find('img', {'alt': True}).attrs["alt"]
+        except:
+            TITLE = "Not Found"
+        try:
+            DATA_ASIN = CARD.attrs["data-asin"]
+        except:
+            DATA_ASIN = "Not Found"
+        try:
+            PRICE_WHOLE = CARD.find('span', class_='a-price-whole').text[0:-1]
+            PRICE_WHOLE = ''.join(PRICE_WHOLE.split('.'))
+        except:
+            PRICE_WHOLE = "Not Found"
+        try:
+            PRICE_FRACTION = CARD.find('span', class_='a-price-fraction').text
+        except:
+            PRICE_FRACTION = "Not Found"
+        try:
+            PRICE_SYMBOL = CARD.find('span', class_='a-price-symbol').text
+        except:
+            PRICE_SYMBOL = "Not Found"
+        try:
+            LINK = "https://www.amazon.com.tr" + str(CARD.find('a', {'href': True}).attrs["href"])
+        except:
+            LINK = "Not Found"
+
+        if TITLE == "Not Found" or DATA_ASIN == "Not Found" or PRICE_WHOLE == "Not Found" or PRICE_FRACTION == "Not Found" or PRICE_SYMBOL == "Not Found" or LINK == "Not Found":
+            pass
+        else:
+            PRICE = PRICE_WHOLE + "." + PRICE_FRACTION
+            DATA = {'data-asin': DATA_ASIN, 'title': TITLE, 'link': LINK, 'price': PRICE, 'price_symbol': PRICE_SYMBOL}
+            PRODUCTS.append(DATA)
+
+def main(URL):
+
+    global URLS, CARDS, PRODUCTS, RESULTS
+
+    URLS = []
+    DATA = {}
+    PRODUCTS = []
+    CARDS = []
+    TASKS_ = []
+    RESULTS = []
+
+#   Retrieving total page number
+    TOTAL_NUMBER_OF_PAGES = number_of_pages(URL)
+#   Iterating and generating urls with page numbers
+    generating_urls(URL, TOTAL_NUMBER_OF_PAGES)
+#   Defining loop and retrieving result htmls
+    start = time.time()
+    #runner(1, TOTAL_NUMBER_OF_PAGES)
+    loop = asyncio.get_event_loop()
+    RESULTS = loop.run_until_complete(async_main())
+    finish = time.time()
+    listing_cards(RESULTS)
+    processing_cards(CARDS)
+    total_time = finish-start
+    #for PRODUCT in PRODUCTS:
+        #print(PRODUCT)
+    #print("Saat : " + str(time.localtime().tm_hour) + "." + str(time.localtime().tm_min) + "\tTaranan ürün sayısı : " + str(len(PRODUCTS)) + "\t" + "\tToplam işlem süresi : " + str(total_time))
+
+    PRODUCT_COUNT = str(len(PRODUCTS))
+    del URLS, DATA, CARDS, TASKS_
+    return PRODUCTS, PRODUCT_COUNT, str(total_time)
+
+if __name__ == "__main__":
+    #URL = "https://www.amazon.com.tr/s?i=pets&rh=n%3A20684004031&fs=true&page=2&qid=1647450852&ref=sr_pg_2"
+    main(URL)
---
+++ db_integration.py
@@ -0,0 +1,34 @@
+import sqlite3
+import aiohttp_parser
+import time
+
+#url = "https://www.amazon.com.tr/s?i=fashion&bbn=13546844031&rh=n%3A13546844031%2Cp_6%3AA1UNQM1SR2CHM%7CA3O5TP4R0OZYXZ%2Cp_n_size_browse-vebin%3A13681704031&dc&fs=true&page=1&pf_rd_i=13546649031&pf_rd_m=A1UNQM1SR2CHM&pf_rd_p=c1d872b6-273d-43fb-9493-093f9cc5ea4f&pf_rd_r=GMVNXJR2E31PA03EMKZP&pf_rd_s=merchandised-search-7&pf_rd_t=101&qid=1646849977&rnid=13663958031&ref=sr_pg_2"
+
+CONN = sqlite3.connect("amazon_database.db")
+CUR = CONN.cursor()
+
+def save_results(Kategori_Adı, RES):
+    TABLE_NAME = Kategori_Adı.replace(' ', '_')
+    CUR.execute("CREATE TABLE IF NOT EXISTS {}(DATA_ASIN text unique,  PRODUCT_TITLE text, PRODUCT_LINK text, CURRENCY text, DISCOUNT integer, OLD_PRODUCT_PRICE integer, NEW_PRODUCT_PRICE integer)".format(TABLE_NAME))
+    CUR.execute("UPDATE {} SET OLD_PRODUCT_PRICE = NEW_PRODUCT_PRICE".format(TABLE_NAME))
+    for PRODUCT in RES[0]:
+        CUR.execute('INSERT OR REPLACE INTO {}(DATA_ASIN, PRODUCT_TITLE, PRODUCT_LINK, NEW_PRODUCT_PRICE, CURRENCY) VALUES(?,?,?,?,?)'.format(TABLE_NAME), (PRODUCT["data-asin"], PRODUCT["title"], PRODUCT["link"], PRODUCT["price"], PRODUCT["price_symbol"]))
+    CUR.execute("UPDATE {} SET DISCOUNT =(SELECT (NEW_PRODUCT_PRICE-OLD_PRODUCT_PRICE)/100 FROM {})".format(TABLE_NAME, TABLE_NAME))
+
+def main():
+    TOTAL_TIME_START = time.time()
+    LIST_TO_PARSE = CUR.execute("SELECT Kategori_Adı, Kategori_Linki FROM Main WHERE Durum = '1'").fetchall()
+    for Kategori_Adı, Kategori_Linki in LIST_TO_PARSE:
+        RES = aiohttp_parser.main(Kategori_Linki)
+        save_results(Kategori_Adı, RES)
+        print("Kategori : " + Kategori_Adı + "\tTaranan ürün sayısı : " + str(RES[1]) + "\t Toplam işlem süresi : " + str(RES[2]))
+        #with open(str(Kategori_Adı)+'.txt', 'w', encoding='utf-8') as f:
+            #for i in RES[0]:
+                #f.write(str(i)+"\n")
+    TOTAL_TIME_END = time.time()
+    print("Bütün kategoriler için geçen toplam süre : " + str(TOTAL_TIME_END-TOTAL_TIME_START))
+
+if __name__ == "__main__":
+    main()
+    CONN.commit()
+    print("commited")
